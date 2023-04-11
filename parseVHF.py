9d0d666eef0a972541c1dc1b7ea81ef0a0891fe3
thormund-nus <e0308991@u.nus.edu>
2023-04-11 17:21:46 +0800

    Optimizes binary VHF parser with numpy
    Class has yet to implement methods for hex and ascii output.

--- parseVHF.py
+++ parseVHF.py
@@ -1,4 +1,4 @@
-import array
+import numpy as np
 import os
 import struct
 
@@ -6,40 +6,30 @@ class VHFparser():
     # Written with reference to SVN r14
 
     def __init__(self, filename: str | os.PathLike):
-        # self.tmp = 0
-        self.num_read_bytes = 0
+        """Takes a VHF output file and populates relevant properties."""
+        self._num_read_bytes = 0
         self.filesize = os.path.getsize(filename) # number of bytes
+
+        # populate header
         with open(filename, 'rb') as file: # binary read
             header = file.read(8)
-            # self.num_read_bytes += 8
-            if not 0xFFFFFFFFFFFF0000 & int.from_bytes(header, 'little')== 0x123456ABCDEF0000:
+            if not 0xFFFFFFFFFFFF0000 & int.from_bytes(header, 'little') == 0x123456ABCDEF0000:
                 raise ValueError('File does not conform to header expectations.')
             
-            header_count_b = (int.from_bytes(header, 'little') & 0xffff) - 1 # 1 to account for first word used to determine size of header
+            # 1 to account for first word used to determine size of header
+            header_count_b = (int.from_bytes(header, 'little') & 0xFFFF) - 1 
             header_count = header_count_b*8
-            self.num_read_bytes += header_count
+            self._num_read_bytes += header_count
             self.header = file.read((header_count)).rstrip(b"\x00")
-            # print(f'{self.header = }\n{len(self.header) = }\n{header_count = }\n{self.num_read_bytes = }')
 
-            max_size = int((self.filesize - self.num_read_bytes)/8)
-            self.i_arr, self.q_arr, self.m_arr = array.array('h', [0]*max_size), array.array('l', [0]*max_size), array.array('l', [0]*max_size)  # not consistent with struct module
-            idx, pop = 0, 0
-            while (word := file.read(8)):
-                # if self.tmp < 4:
-                #     print(f"{word = }")
-                self.num_read_bytes += 8
-                if word == b'\x00\x00\x00\x00\x00\x00\x00\x00':
-                    pop += 1
-                    continue
-                self.read_word(word, idx)
-                del word
-                idx += 1
-                # self.tmp += 1
-                # if self.tmp > 11:
-                #     break
-
-            # print(f"{pop = }")
-            [(self.i_arr.pop(), self.q_arr.pop(), self.m_arr.pop()) for _ in range(pop)]
+        # populate body "data"
+        max_data_size = int((self.filesize - self._num_read_bytes)/8)
+        self.data = np.memmap(filename, 
+                              dtype=np.dtype(np.uint64).newbyteorder('<'), 
+                              mode='r', offset=self._num_read_bytes, 
+                              shape=(max_data_size,))
+        # get (I, Q, M)
+        self.read_words_numpy(self.data)
 
     def read_word(self, b: bytes, idx = int):
         """Converts 8 bytes in (I,Q, M)."""
@@ -57,18 +47,28 @@ class VHFparser():
 
         # substitute for non-local variable
         self.m_arr[idx] = m
-        # try:
         self.i_arr[idx] = i
-        # except OverflowError as exc:
-        #     print(f"{b = }")
-        #     print(f"{struct.unpack_from('<I', b, 2)[0] >> 8 = }")
-        #     print(f"{i = }")
-        #     print(f"{struct.unpack_from('<I', b, 3)[0] & 0xFFFFFF = }")
-        #     print(exc)
-        #     os.exit(-1)
-
         self.q_arr[idx] = q
+    
+    def read_words_numpy(self, data: np.ndarray):
+        self.i_arr = np.bitwise_and(np.right_shift(data, 24), 0xFFFFFF, dtype=np.dtype(np.int32))
+        self.i_arr = self.i_arr - (self.i_arr >> 23) * 2**24
+        self.q_arr = np.bitwise_and(data, 0xFFFFFF, dtype=np.dtype(np.int32))
+        self.q_arr = self.q_arr - (self.q_arr >> 23) * 2**24
+        self.m_arr = np.right_shift(data, 48, dtype=np.dtype(np.int64))
+        # self.m_arr = self._m_arr_copy.astype(np.int16)
 
+        # failed alternative to m_arr creation
+        # self.m_arr.dtype = np.int16 # in-place data change, may be depreciated
+        # ? setting the dtype seems to be as buggy as ndarray.view() method
+        # this is in contrast to ndarray.astype() method, which returns a copy
+        return
+
+    # @property
+    # def m_arr(self):
+    #     return self._m_arr_copy.view(dtype=np.int16)
+
+    
 if __name__ == '__main__':
     # x = VHFparser(os.path.join(os.path.dirname(__file__), 'vhf_func_gen/Data/60.000_020MHz.txt'))
     # print(f'{x.header = }')
@@ -77,5 +77,5 @@ if __name__ == '__main__':
     print()
     y = VHFparser(os.path.join(os.path.dirname(__file__), 'Data/2023-04-05T16:56:09.551460_s4_q10000000.txt'))
     # print(f'{y.header = }')
-    # print(f'{y.i_arr[:12] = }')
+    # print(f'{y.i_arr[:12] = }\n{y.m_arr[:12] = }')
     # print(f"{y.i_arr[-5:] = }\n{y.m_arr[-5:] = }")
