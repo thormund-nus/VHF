Introduce fix for 16-bit overflow of m_arr

parseVHF.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from functools import cached_property
 import logging
 import numpy as np
 import os
@@ -9,11 +10,32 @@ from _io import BufferedRandom
 
 class VHFparser:
     # Written with reference to SVN r14
+    """ 
+    Class that parses binary, hexadecimal and ASCII output from VHF board,
+    from file objects and data streams.
+
+    Init-time available properties
+    -----
+    filesize: Number of bytes 
+    header: Dictionary containing all paramaters used to call runVHF
+
+    Available methods
+    -----
+    parse_header: Gets relevant front number of bytes and returns 
+    read_words_numpy: Converts binary words into (Q,I,M) arrays
+
+    Run-time available properties
+    -----
+    reduced_phase: Phase/2pi array
+    """
 
     def __init__(self, filename: str | os.PathLike | BufferedRandom):
-        """Takes a VHF output file and populates relevant properties."""
-        self.__createLogger()
+        """Take a VHF output file and populates relevant properties."""
+        self.__create_logger()
+        self.logger.info('VHF parser has been run for file: %s', filename)
+        # Create class specific modifiers
         self._num_read_bytes = 0
+        self._fix_m_called = False
 
         # Begin Parsing logic
         if isinstance(filename, BufferedRandom):
@@ -40,14 +62,14 @@ class VHFparser:
         # get (I, Q, M)
         self.read_words_numpy(self.data)
 
-    def __createLogger(self):
+    def __create_logger(self):
         self.logger = logging.getLogger("vhfparser")
-    
+
     def _init_file(self, filename):
         """For files. Opens file object, and parse by _init_buffer."""
         with open(filename, "rb") as file:  # binary read
             self._init_buffer(file)
-    
+
     def _init_buffer(self, buffer):
         """For bufferedRandom."""
         header = buffer.read(8)
@@ -64,27 +86,36 @@ class VHFparser:
         self.headerraw: bytes = buffer.read((header_count)).rstrip(b"\x00")
         self.parse_header(self.headerraw)
 
-
     # def read_word(self, b: bytes, idx=int):
     #     """converts 8 bytes in (I, Q, M)."""
     #     m = struct.unpack_from('<h', b, 6)[0]
     #     i = struct.unpack_from('<i', b, 2)[0] >> 8
     #     # i = struct.unpack_from('<i', b, 3)[0] & 0xffffff
     #     q = struct.unpack_from('<i', b, 0)[0] & 0xffffff
-
+    #
     #     sign_extend = -1 & ~0x7fffff
     #     if i & 0x800000:
     #         i |= sign_extend
     #     # i = i - (i >> 23) * 2**24 # branchless
     #     if q & 0x800000:
     #         q |= sign_extend
-
+    #
     #     # substitute for non-local variable
     #     self.m_arr[idx] = m
     #     self.i_arr[idx] = i
     #     self.q_arr[idx] = q
 
-    def read_words_numpy(self, data: np.ndarray):
+    def read_words_numpy(self, data: np.ndarray, fix_m: bool = False):
+        """Convert binary words into numpy arrays.
+
+        Input
+        -----
+        data: np.ndarray
+            This contains numpy binary data that contains 1 word of data
+            per array element.
+        fix_m: bool
+            Accounts for 16-bit overflow of m during reading.
+        """
         self.i_arr = np.bitwise_and(
             np.right_shift(data, 24), 0xFFFFFF, dtype=np.dtype(np.int32)
         )
@@ -98,13 +129,12 @@ class VHFparser:
         # self.m_arr.dtype = np.int16 # in-place data change, may be depreciated
         # ? setting the dtype seems to be as buggy as ndarray.view() method
         # this is in contrast to ndarray.astype() method, which returns a copy
-        return
 
-    # @property
-    # def m_arr(self):
-    #     return self._m_arr_copy.view(dtype=np.int16)
+        if fix_m:
+            self._fix_overflow_m()
 
     def parse_header(self, header_raw: bytes):
+        """Bytes as read from header of bin files is converted into a header property."""
         if header_raw is None:
             raise ValueError("header_raw was not given.")
 
@@ -114,12 +144,10 @@ class VHFparser:
         print(f"Debug: {header_raw = }")
         for x in header_raw[0].split(b" -")[1:]:  # command line record
             x = x.decode().strip(" ")
-            # print(f"Debug: {x = }")
             self.header[x[0]] = x[1:].strip()
-            # print(f"Debug: {self.header[x[0]] = }")
 
-        # print(f"Debug: {header_raw[1].split(b': ')[1].decode().strip() = }")
-        self.header['Time start'] = datetime.fromisoformat(header_raw[1].split(b': ')[1].split(b'\n')[0].decode().strip())
+        self.header["Time start"] = datetime.fromisoformat(
+            header_raw[1].split(b': ')[1].split(b'\n')[0].decode().strip())
         for k, v in self.header.items():
             try:
                 self.header[k] = int(v)
@@ -129,17 +157,66 @@ class VHFparser:
                 pass
 
         # generate explicit params
-        if "l" in self.header.keys():
+        if 'l' in self.header:
             self.header["base sampling freq"] = 10e6
-        elif "h" in self.header.keys():
+        elif 'h' in self.header:
             self.header["base sampling freq"] = 20e6
         else:
             self.header["base sampling freq"] = 20e6
 
-        if self.header["s"] is not None:
+        if self.header['s'] is not None:
             self.header["sampling freq"] = self.header["base sampling freq"] / (
                 1 + self.header["s"]
             )
+
+    @cached_property
+    def _potential_m_overflow(self) -> bool:
+        """Return true if there exists elements in self.m_arr close exceeds +-tol."""
+        # Used to calibrate invokation of _m_fix()
+        tol: int = 0x7F00
+        if tol < 0:
+            raise ValueError
+        if np.size(np.where(self.m_arr > tol)) + np.size(np.where(self.m_arr < -tol)) > 0:
+            return True
+        return False
+
+    def _fix_overflow_m(self) -> None:
+        """Class method for fixing m value with 16-bit overflow."""
+        if not self._fix_m_called:
+            # get m_shift of +- n as according before multiplying by 2**16 to
+            # account for overflow
+
+            # 1. get location of +/-ve overflows
+            # index 0 being +1 would mean that m[0] has flowed above the 16-bit
+            # limit into a negative m[1], similarly, index 1 being -1 would
+            # mean that m[1] has flowed below the 16-bit minimum into a large
+            # positive m[2]
+            deltas = np.diff(self.m_arr)  # subtraction between views
+            # 2. now round towards +-1
+            tol = 0xF000  # keep as int
+            deltas = np.greater(deltas, tol).astype(
+                int) - np.less(deltas, -tol).astype(int)
+            # 3. finally fix
+            self.m_arr[1:] -= 0x10000 * np.cumsum(deltas)
+            self._fix_m_called = True
+        else:
+            print("overflow m_arr has been fixed before!")
+
+    @cached_property
+    def reduced_phase(self):
+        """Obtains phase(time)/2pi for the given file.
+
+        phase is obtained by atan(Q/I).
+        """
+
+        # Account for 16 bit overflow of m.
+        if not self._fix_m_called and self._potential_m_overflow:
+            self._fix_overflow_m()
+
+        phase = -np.arctan2(self.i_arr, self.q_arr)
+        phase /= 2*np.pi
+        phase -= self.m_arr
+        return phase
 
 
 if __name__ == "__main__":