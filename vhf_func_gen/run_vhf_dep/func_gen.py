34c1e15e5d3430bba43ffda442f23d98d1be9669
thormund-nus <e0308991@u.nus.edu>
2024-02-02 11:58:24 +0800

    Add default channel for function generator
    With proper type hinting as to "my_resource" attribute, it was
    discovered that the overloading of write and query methods should have
    been independent of the serial interface method. This is rectified along
    with default channel being part of class initialization, with a name
    change to emphasise on that the class was designed for single channel
    operation in mind.

--- vhf_func_gen/run_vhf_dep/func_gen.py
+++ vhf_func_gen/run_vhf_dep/func_gen.py
@@ -2,15 +2,17 @@
 
 from functools import wraps
 from logging import basicConfig, getLogger, DEBUG
+from typing import Optional
 from pyvisa import ResourceManager  # LecroyDSO uses the pyvisa library
-from pyvisa.resources import Resource
-from qodevices.baseclass.baseserial import serial_comm
+from pyvisa.resources import USBInstrument
 from time import sleep
 
 
-class SyncFuncGen(serial_comm):
+class SyncSingleChannelFuncGen():
+    """Tektronix Function Generator Single Channel synchrous operation wrapper."""
+
     def __init__(self, rsc_str: str = '', channel: str = '1', timeout: float = 2) -> None:
-        """Synchrous Tektronix Function Generator instance.
+        """Synchrous Tektronix Function Generator instance with single channel.
 
         Input
         -----
@@ -19,7 +21,9 @@ class SyncFuncGen(serial_comm):
         """
         # Manual obtainable from: https://www.tek.com/en/signal-generator/afg3000-manual/afg3000-series-1
         rm = ResourceManager()
-        self.my_resource: Resource = rm.open_resource(rsc_str)
+        self.my_resource: USBInstrument = rm.open_resource(
+            rsc_str, open_timeout=timeout
+        )
         self.__init_logger()
         self.logger.info(
             "Resource has been opened. Close connection by using del on instance.")
@@ -31,14 +35,6 @@ class SyncFuncGen(serial_comm):
             return getattr(self.my_resource, __name)
         else:
             raise AttributeError(f"{__name=}")
-
-    # @property
-    def ask(self, string):
-        return self.my_resource.query(string).strip()
-
-    @property
-    def write(self):
-        return self.my_resource.write
 
     def __del__(self) -> None:
         if self.my_resource._session is not None:
@@ -58,18 +54,35 @@ class SyncFuncGen(serial_comm):
             return result
         return func_wrap
 
-    def assert_sinusoidal_mode(self, channel: str = '1'):
+    def ask(self, string):
+        """PyVisa query method."""
+        return self.my_resource.query(string).strip()
+
+    @log_query
+    def write(
+        self, message: str, termination: str | None = None, encoding: str | None = None
+    ) -> int:
+        """Jank write redirector using property attribute."""
+        self.logger.debug("write called with: message=%s, termination=%s, encoding=%s",
+                          message, termination, encoding)
+        return self.my_resource.write(message=message, termination=termination, encoding=encoding)
+
+    def assert_sinusoidal_mode(self, channel: Optional[str | int] = None):
+        if channel is None:
+            channel = self.channel
         self.beeper_q(channel)
         try:
             assert int(self.am_q(channel)) == 0
         except AssertionError:
-            self.am(channel, 0)
+            self.am(0, channel)
         assert int(self.fm_q(channel)) == 0
         assert int(self.pm_q(channel)) == 0
         assert int(self.pwm_q(channel)) == 0
 
-    def prepare_sinusoidal(self, channel: str = '1'):
+    def prepare_sinusoidal(self, channel: Optional[str | int] = None):
         """Ensure channel output is sinusoidal"""
+        if channel is None:
+            channel = self.channel
         self.write("SOURce:ROSCillator:SOURce EXTernal")  # reference clock
         self.write(f"OUTPut{channel}:IMPedance 50 ohms")  # Sets impendance
         self.write(f"OUTPUT{channel}:STATE ON")  # Turns output on
@@ -79,41 +92,58 @@ class SyncFuncGen(serial_comm):
         self.logger.info("prepare_sinusoidal completed!")
 
     @log_query
-    def beeper_q(self, channel: str = '1'):
+    def beeper_q(self, channel: Optional[str | int] = None):
+        if channel is None:
+            channel = self.channel
         return self.ask("SYSTem:BEEPer:STATe?")
 
     @log_query
-    def am_q(self, channel: str = '1'):
+    def am_q(self, channel: Optional[str | int] = None):
+        if channel is None:
+            channel = self.channel
         return self.ask(f"SOURce{channel}:AM:STATe?")
 
-    def am(self, channel: str = '1', val: str = '0'):
+    def am(self, val: str | int = '0', channel: Optional[str | int] = None):
+        if channel is None:
+            channel = self.channel
         return self.ask(f"SOURce{channel}:AM:STATe {val}")
 
     @log_query
-    def pm_q(self, channel: str = '1'):
+    def pm_q(self, channel: Optional[str | int] = None):
+        if channel is None:
+            channel = self.channel
         return self.ask(f"SOURce{channel}:PM:STATe?")
 
     @log_query
-    def fm_q(self, channel: str = '1'):
+    def fm_q(self, channel: Optional[str | int] = None):
+        if channel is None:
+            channel = self.channel
         return self.ask(f"SOURce{channel}:FM:STATe?")
 
     @log_query
-    def pwm_q(self, channel: str = '1'):
-        """Pulse setting on Func Gen. Corresponds to Pulse Width Modulated."""
+    def pwm_q(self, channel: Optional[str | int] = None):
+        """Query Pulse setting on Func Gen. Corresponds to Pulse Width Modulated."""
+        if channel is None:
+            channel = self.channel
         return self.ask(f"SOURce{channel}:PWM:STATe?")
 
-    def get_min_freq(self, channel: str = '1'):
+    def get_min_freq(self, channel: Optional[str | int] = None):
         """Get the minimum settable frequency."""
+        if channel is None:
+            channel = self.channel
         # Have to account for the side effect that MIN can only be obtained via setting
         tmp_freq = self.ask(f"SOURce{channel}:FREQuency?")
         self.write(f"SOURce{channel}:FREQuency MINimum")
         result = self.ask(f"SOURce{channel}:FREQuency?")
         self.write(f"SOURce{channel}:FREQuency {tmp_freq}")
-        assert tmp_freq == self.ask(f"SOURce{channel}:FREQuency?")
+        assert float(tmp_freq) == float(
+            self.ask(f"SOURce{channel}:FREQuency?"))
         return result
 
-    def get_max_freq(self, channel: str = '1'):
+    def get_max_freq(self, channel: Optional[str | int] = None):
         """Get the maximum settable frequency."""
+        if channel is None:
+            channel = self.channel
         # Have to account for the side effect that MAX can only be obtained via setting
         tmp_freq = self.ask(f"SOURce{channel}:FREQuency?")
         self.write(f"SOURce{channel}:FREQuency MAXimum")
@@ -122,29 +152,48 @@ class SyncFuncGen(serial_comm):
         assert tmp_freq == self.ask(f"SOURce{channel}:FREQuency?")
         return result
 
-    def set_freq(self, channel: str = '1', freq: str = "60.000MHz"):
-        """Sets the channel to the specified frequency. No checking is done."""
+    def set_freq(self, freq: str = "60.000MHz", channel: Optional[str | int] = None):
+        """Set the channel to the specified frequency. No checking is done."""
+        if channel is None:
+            channel = self.channel
         self.write(f"SOURce{channel}:FREQuency {freq}")
 
     @log_query
-    def volt_lim_low_q(self, channel: str = '1'):
+    def volt_lim_low_q(self, channel: Optional[str | int] = None):
+        if channel is None:
+            channel = self.channel
         return self.ask(f"SOURce{channel}:VOLTage:LIMit:LOW?")
 
     @log_query
-    def volt_lim_high_q(self, channel: str = '1'):
+    def volt_lim_high_q(self, channel: Optional[str | int] = None):
+        if channel is None:
+            channel = self.channel
         return self.ask(f"SOURce{channel}:VOLTage:LIMit:HIGH?")
 
-    def set_amplitude(self, channel: str = '1', ampl_vpp: str = '1.0'):
-        """Sets the channel to the specified peak-to-peak voltage. No checking is done.
+    def set_amplitude(self, ampl_vpp: float = 1.0, channel: Optional[str | int] = None):
+        """Set the channel to the specified peak-to-peak voltage. No checking is done.
 
         This setting has a resolution of 0.1 mVpp.
+
+        Inputs
+        ------
+        ampl_vpp: float
+        channel: str
         """
-        self.logger.debug("Setting channel %s to %sVpp.", channel, ampl_vpp)
+        if channel is None:
+            channel = self.channel
+        self.logger.debug(
+            "Setting channel %s to %sVpp. Note that if too large or small, it will not be set.",
+            channel,
+            ampl_vpp
+        )
         self.write(f"SOURce{channel}:VOLTage:LEVel:IMMediate:AMPLitude {ampl_vpp}Vpp")  # noqa
         self.logger.debug("Setted.")
         # assert float(ampl_vpp) == (
         #     res := float(self.get_amplitude(channel))), f"result={res}"
 
     @log_query
-    def get_amplitude(self, channel: str = '1'):
+    def get_amplitude(self, channel: Optional[str] = None):
+        if channel is None:
+            channel = self.channel
         return self.ask(f"SOURce{channel}:VOLTage:LEVel:IMMediate:AMPLitude?")
