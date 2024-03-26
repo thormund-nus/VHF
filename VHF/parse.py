Fix hypotenuse integer overflow from int32 limit in squaring
Especially for situations where r(t) was very large, and hence either
I(t) or Q(t) would be very large, I.dtype=np.dtype(np.int32) meant that
np.power(I, 2) would have been negative, leading to unexpected
behaviour.

In the context of dev-VHFcheck, this meant that child processes were
killed during the plotting process, resulting in the creation of more
child processes then were necessary.

VHF/parse.py
@@ -367,5 +367,6 @@ class VHFparser:
 
         Radius is obtained by sqrt(Q^2 + I^2).
         """
-        radii = np.sqrt(np.power(self.q_arr, 2) + np.power(self.i_arr, 2))
+        # dtype starting from int32 should be a non-issue
+        radii = np.hypot(self.q_arr, self.i_arr)
         return radii