116740e8b3ebb91751e8ae1854e548b562b9c222
thormund-nus <e0308991@u.nus.edu>
2024-07-04 10:40:27 +0800

    Add unit test to ensure consistency with C code
    The reference as given by the C file is tested against in
    test/test_parseBinaryVHFTrace.py.
    Git submodules isn't used here, just place the C file in
    test/usbhybrid-reference/....
    Unsurprisingly, BinaryVHFTrace as a class is consistent with the C
    reference.

---
+++ test/test_parseBinaryVHFTrace.py
@@ -0,0 +1,132 @@
+import pytest
+import logging
+import numpy as np
+from numpy.typing import NDArray
+import os
+from pathlib import Path
+import subprocess
+import sys
+from typing import Callable
+module_path = str(Path(__file__).parents[1])
+if module_path not in sys.path:
+    sys.path.append(module_path)
+from VHF.parse import BinaryVHFTrace
+
+compiled_path = "usbhybrid-reference/convert_bin_to_text"
+compiled = Path(__file__).parent.joinpath(compiled_path)
+compile_src = Path(__file__).parent.joinpath(compiled_path+".c")
+compiled_pwd = str(compiled.relative_to(Path(os.getcwd())))
+
+
+def test_CompiledC_exists():
+    """Checks for standard reference before doing other tests."""
+    if compiled.exists():
+        logging.info("Compiled bin_to_text found")
+        assert True
+        return
+    else:
+        logging.info("Compiled bin_to_text not found")
+        if compile_src.exists():
+            logging.info("Trying to compile from %s", compile_src)
+            subprocess.run(
+                ["gcc", "-g", "-O3", "-o", compiled_path, compiled_path+".c"],
+                check=True
+            )
+            assert compiled.exists()
+
+        else:
+            logging.error(
+                "Please try to get the C file from usbhybrid repo and place "
+                "into test/usbhybrid-reference!"
+            )
+            assert False
+    return
+
+
+def q_from_stdout(x: str) -> list[str]:
+    return x.split()[1::3]
+
+
+def i_from_stdout(x: str) -> list[str]:
+    return x.split()[0::3]
+
+
+def m_from_stdout(x: str) -> list[str]:
+    return x.split()[2::3]
+
+
+def compiled_output(
+    std_input, filt: Callable[[str], list[str]]
+) -> NDArray[np.int32]:
+    """See what convert_bin_to_text returns."""
+    # logging.debug("compiled_pwd = %s", compiled_pwd)
+    process = subprocess.run(
+        [compiled_pwd],
+        input=std_input,
+        stdout=subprocess.PIPE
+    )
+    s_out = process.stdout.decode()
+    result = np.array(filt(s_out), dtype=np.int32)
+    return result
+
+
+def range_to_Q_range(start, end) -> NDArray:
+    """NDArray inclusive of start and end for Q."""
+    assert 0 <= start <= 0xFFFFFF
+    assert 0 <= end <= 0xFFFFFF
+    result = np.arange(start, end+1, dtype=np.uint64)
+    result.astype("<S8")
+    return result
+
+
+def range_to_I_range(start, end) -> NDArray:
+    """NDArray inclusive of start and end for I."""
+    assert 0 <= start <= 0xFFFFFF
+    assert 0 <= end <= 0xFFFFFF
+    steps = 0x1000000
+    result = np.arange(start*steps, (end+1)*steps, step=steps, dtype=np.uint64)
+    result.astype("<S8")
+    return result
+
+
+def range_to_M_range(start, end) -> NDArray:
+    """NDArray inclusive of start and end for I."""
+    assert 0 <= start <= 0xFFFF
+    assert 0 <= end <= 0xFFFF
+    steps = 0x1000000000000
+    result = np.arange(start*steps, (end+1)*steps, step=steps, dtype=np.uint64)
+    result.astype("<S8")
+    return result
+
+
+def test_Q():
+    """Check that unwrapping of bottom 24 bits are equivalent."""
+    chunk_size = 16*1024  # We pass in a range of 16x C's buffer size
+    max_j = int(0xFFFFFF / chunk_size)
+    for j in range(max_j):
+        q_under_test = range_to_Q_range(j*chunk_size, (j+1)*chunk_size)
+        result = BinaryVHFTrace.read_q_arr(q_under_test.astype("<u8"))
+        expected = compiled_output(q_under_test.tobytes(), q_from_stdout)
+        assert np.all(result == expected), f"test_Q failed at {j =}"
+
+
+def test_I():
+    """Check that unwrapping of middle 24 bits are equivalent."""
+    chunk_size = 16*1024  # We pass in a range of 16x C's buffer size
+    max_j = int(0xFFFFFF / chunk_size)
+    for j in range(max_j):
+        i_under_test = range_to_I_range(j*chunk_size, (j+1)*chunk_size)
+        result = BinaryVHFTrace.read_i_arr(i_under_test.astype("<u8"))
+        expected = compiled_output(i_under_test.tobytes(), i_from_stdout)
+        assert np.all(result == expected), f"test_I failed at {j =}"
+
+
+def test_M():
+    """Check that unwrapping of top 16 bits are equivalent."""
+    chunk_size = 16*1024  # We pass in a range of 16x C's buffer size
+    max_j = int(0xFFFF / chunk_size)
+    for j in range(max_j):
+        m_under_test = range_to_M_range(j*chunk_size, (j+1)*chunk_size)
+        result = BinaryVHFTrace.read_m_arr(m_under_test.astype("<u8"))
+        expected = compiled_output(m_under_test.tobytes(), m_from_stdout)
+        assert np.all(result == expected), f"test_M failed at {j =}"
