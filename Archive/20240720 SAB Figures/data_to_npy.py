f392344ab8acccaf91688d7817bc6431a67171c9
thormund-nus <e0308991@u.nus.edu>
2024-08-15 16:31:52 +0800

    Use naive datetimes for digestor
    As mentioned in the prior commit, the removal of Pandas has made it abit
    easier to reason about the available methods as the type of object
    (datetime/np.datetime64/~~pandas.Timestamp~~). This commit imposes
    timings_left() to return naive datetime.datetime (in contrast to the
    previous commit's failed attempt setting it np.datetime64). However,
    there's still a bit of redundant aware_to_naive being done to interact
    with the fact that the entire infrastructure has to now work around the
    user-desired data point being (timezone-)naive.

--- Archive/20240720 SAB Figures/data_to_npy.py
+++ Archive/20240720 SAB Figures/data_to_npy.py
@@ -10,6 +10,7 @@ from numpy.typing import NDArray
 from pathlib import Path
 from scipy.signal import decimate
 import sys
+from typing import Iterable
 module_path = str(Path(__file__).parents[2])
 if module_path not in sys.path:
     sys.path.append(module_path)
@@ -38,6 +39,7 @@ def init_pool_processes(the_lock):
 
 def worker(args):
     """Currently, we demand that workers can only take 1 arg for imap."""
+    logger.info("worker called with args: %s", args)
     return averaging_spectrogram(args)
 
 @cache
@@ -48,41 +50,40 @@ def trace_endpoints(file) -> tuple[datetime, datetime]:
     end_time: datetime = p.timings.trace_end
     return (start_time, end_time)
 
-def get_bin_files(time: np.datetime64) -> list[Path]:
+def get_bin_files(time: datetime) -> list[Path]:
     """Obtain set of files to iterate over for time to time+interval.
 
     Files are included iff the provided timing is at least half of the desired
     interval.
     """
 
-    def time_is_located_within_file_endpoints(time: np.datetime64, file):
-        start_time, end_time = map(datetime64, map(aware_to_naive, trace_endpoints(file)))
+    def time_is_located_within_file_endpoints(time: datetime, file):
+        start_time, end_time = map(aware_to_naive, trace_endpoints(file))
         return start_time <= time and time <= end_time
 
     start_side = list(filter(lambda x: time_is_located_within_file_endpoints(time, x), FILES))
-    end_side = list(filter(lambda x: time_is_located_within_file_endpoints(time+timedelta64(INTERVAL), x), FILES))
+    end_side = list(filter(lambda x: time_is_located_within_file_endpoints(time + INTERVAL, x), FILES))
     result: list[Path] = start_side
     result.extend(end_side)
     result: set[Path] = set(result)
     if len(result) == 0 or len(result) > 2:
-        logger.warn(f"No file found for {time=}. {len(result)=}")
+        logger.warning("No file found for time=%s. {len(result)=%s}", time, len(result))
         pass
     if len(result) == 1:
         start, end = trace_endpoints(min(result))  # stupid way to access set item without pop
-
-        start, end = map(datetime64, map(aware_to_naive, [start, end]))
+        start, end = map(aware_to_naive, [start, end])
         # pop if overlap is less than helf
-        if start <= time + timedelta64(INTERVAL) <= end:
-            if start - time > timedelta64(INTERVAL)/2:
+        if start <= time + INTERVAL <= end:
+            if start - time > INTERVAL/2:
                 result.pop
         elif start <= time <= end:
-            if end - time < timedelta64(INTERVAL)/2:
+            if end - time < INTERVAL/2:
                 result.pop
 
     return sorted(list(result))
 
 
-def averaging_spectrogram(time: np.datetime64):
+def averaging_spectrogram(time: datetime):
     """We get the spectrogram for (time, time+INTERVAL), and save into npy file."""
     files = get_bin_files(time)
     logger.debug("Proc %s got files = %s", current_proc().name, map(lambda x: x.name, files))
@@ -93,7 +94,7 @@ def averaging_spectrogram(time: np.datetime64):
     p_freq_old = None
     p_freq = None
 
-    iteration_end = time + timedelta64(INTERVAL)
+    iteration_end = time + INTERVAL
     current_file = files.pop(0)
     parse = VHFparser(current_file, headers_only=True)
     current_file_start = parse.timings.trace_start
@@ -106,12 +107,12 @@ def averaging_spectrogram(time: np.datetime64):
     while plot_start < iteration_end and current_file is not None:
         plot_start = max(
             plot_start,
-            datetime64(aware_to_naive(current_file_start+timedelta(seconds=1)))
+            aware_to_naive(current_file_start+timedelta(seconds=1))
         )  # clamp to at least the first second after any trace
-        plot_end = plot_start + timedelta64(SPECGRAM_TOTAL_WINDOW)
+        plot_end = plot_start + SPECGRAM_TOTAL_WINDOW
         parse.update_plot_timing(
-            start=datetime(plot_start, tzinfo=timedelta(hours=8)),  # fails
-            end=datetime(plot_end, tzinfo=timedelta(hours=8))
+            start=plot_start.astimezone(tz=timezone(timedelta(hours=8))),
+            end=plot_end.astimezone(tz=timezone(timedelta(hours=8)))
         )
         phase = parse.reduced_phase
         freq = parse.header["sampling freq"]
@@ -151,7 +152,7 @@ def averaging_spectrogram(time: np.datetime64):
         # second addition is to move forward by one p-overlap
         plot_start = plot_end - 0.9*timedelta(seconds=SPECGRAM_WINDOW)
         logger.debug("Proc %s moving onto the next iteration of spectrogram window", current_proc().name)
-        if plot_start > current_file_end:
+        if plot_start > aware_to_naive(current_file_end):
             logger.debug("Proc %s attempting to fetch next file", current_proc().name)
             if len(files) == 0:
                 current_file = None
@@ -182,7 +183,7 @@ def averaging_spectrogram(time: np.datetime64):
     logger.debug("Proc %s moving onto next timer!", current_proc().name)
     return
 
-def averaging_spectrogram_write(time, freqs: NDArray, avgSxx: NDArray):
+def averaging_spectrogram_write(time: datetime, freqs: NDArray, avgSxx: NDArray):
     """Takes the data pulled out from the files for specified time and writes into npy file."""
     try:
         with lock:
@@ -225,11 +226,11 @@ def averaging_spectrogram_write_core(time, freqs: NDArray, avgSxx: NDArray):
 def main():
     """Perform digesting of files down into averaged spectrograms."""
     print(f"We are now running for {len(SEARCH_FILES) = }")
-    timings: NDArray[np.datetime64] = timings_left()
+    timings: Iterable[datetime] = timings_left()
     random.shuffle(timings)
 
     npz_lock = Lock()
-    with Pool(cpu_count()-2, initializer=init_pool_processes, initargs=(npz_lock,)) as pool:
+    with Pool(cpu_count()-1, initializer=init_pool_processes, initargs=(npz_lock,)) as pool:
         p = pool.imap_unordered(
             worker,
             # pair_lock_with_time(timings, mutex_lock)
@@ -240,7 +241,9 @@ def main():
 def main_linear():
     """Perform digesting of files down into averaged spectrograms without Pool."""
     print(f"We are now running for {len(SEARCH_FILES) = }")
-    timings: NDArray[np.datetime64] = timings_left()
+    timings: Iterable[datetime] = timings_left()
+    random.shuffle(timings)
+
     lock = Lock()
     for t in timings:
         worker(t)
@@ -260,7 +263,12 @@ def aware_to_naive(t: datetime) -> datetime:
     return result.replace(tzinfo=None)
 
 
-def timings_left() -> NDArray[np.datetime64]:
+def datetime64_to_dt(t: np.datetime64) -> datetime:
+    """Takes a numpy datetime64 and give a naive python datetime."""
+    return datetime.strptime(t.astype(str)[:-3], '%Y-%m-%dT%H:%M:%S.%f')
+
+
+def timings_left() -> list[datetime]:
     """Array of time-axis data points still needed."""
     # np.arange will preferably generate np.datetime64 from TIME_START: datetime.datetime
     # this will consequently cast from TZ+X to TZ+0.
@@ -282,7 +290,8 @@ def timings_left() -> NDArray[np.datetime64]:
     except ValueError:
         logger.debug("Nothing from file")
     only_in_timings = np.setdiff1d(timings, times_arr)
-    logger.debug("Remaining times found needed doing: only_in_timings = %s", only_in_timings)
+    only_in_timings = list(map(datetime64_to_dt, only_in_timings))  # putting this in an NDArray only undoes our work
+    logger.info("Remaining times found needed doing: only_in_timings = %s", only_in_timings)
     return only_in_timings
 
 
